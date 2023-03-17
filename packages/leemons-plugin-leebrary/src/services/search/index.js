1809906dda8db9f1bb2370b60154daa7d2c719ca
MIGUELez11 <miguel@leemons.io>
2023-03-17 14:23:29 +0100

    fix(leebrary): Add mongo ObjectId regex for category

--- packages/leemons-plugin-leebrary/src/services/search/index.js
+++ packages/leemons-plugin-leebrary/src/services/search/index.js
@@ -87,6 +87,7 @@ async function search(
       let _category;
 
       if (
+        category.match(/^[0-9a-fA-F]{24}$/) ||
         category.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)
       ) {
         // eslint-disable-next-line no-param-reassign
